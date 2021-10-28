40dc02a480db1dca1102899ec33b35ecbf9b4a8a
Tetyana Yarish <avamkuda@gmail.com>
2021-10-28 17:10:50 +1100

    Add project files.

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+
+        //[HttpGet]
+        //public IEnumerable<User> Get()
+        //{
+        //    var items = new List<User>();
+        //    using (StreamReader r = new StreamReader("data/user.json"))
+        //    {
+        //        string json = r.ReadToEnd();
+        //        items = JsonConvert.DeserializeObject<List<User>>(json);
+        //    }
+        //    return items;
+        //}
+
+        [HttpGet]
+        public ActionResult<User> GetUserById ([ FromQueryAttribute ]  int ID)
+        {
+            var users = new List<User>();
+            using (StreamReader r = new StreamReader("data/user.json"))
+            {
+                string json = r.ReadToEnd();
+                users = JsonConvert.DeserializeObject<List<User>>(json);
+            }
+            foreach (var user in users)
+            {
+                if (user.ID==ID)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ WebAPI/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebAPI.Controllers
+{
+    public class User
+    {
+        // public Guid ID { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string FamilyName { get; set; }
+        public int Age { get; set; }
+    }
+}
