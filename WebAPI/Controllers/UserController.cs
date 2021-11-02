8ae9b2a1775c79e6282efb97d5e93beeda710bb7
Tetyana Yarish <avamkuda@gmail.com>
2021-11-02 21:23:17 +1100

    New user controller created

--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft;
 using Newtonsoft.Json;
+using System.Xml.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace WebAPI.Controllers
 {
@@ -14,39 +17,65 @@ namespace WebAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-
+        List<User> nUser = new List<User>();
 
         [HttpGet]
         public IEnumerable<User> Get()
         {
-            var items = new List<User>();
+
             using (StreamReader r = new StreamReader("data/user.json"))
             {
                 string json = r.ReadToEnd();
-                items = JsonConvert.DeserializeObject<List<User>>(json);
+                nUser = JsonConvert.DeserializeObject<List<User>>(json);
             }
-            return items;
+            return nUser;
         }
 
         [HttpGet("{ID}")]
-      
-       // public IActionResult GetUserById ([ FromQueryAttribute ]  int ID)
-        public IActionResult GetUserById (int ID)
+
+        // public IActionResult GetUserById ([ FromQueryAttribute ]  int ID)
+        public IActionResult GetUserById(int ID)
         {
-            var users = new List<User>();
+
             using (StreamReader r = new StreamReader("data/user.json"))
             {
                 string json = r.ReadToEnd();
-                users = JsonConvert.DeserializeObject<List<User>>(json);
+                nUser = JsonConvert.DeserializeObject<List<User>>(json);
             }
-            foreach (var user in users)
+            foreach (var user in nUser)
             {
-                if (user.ID==ID)
+                if (user.ID == ID)
                 {
                     return Ok(user);
                 }
             }
             return NotFound();
         }
+        [HttpPost]
+        public async Task<User> PostAsync(User newUser) //Not 
+        {
+            //using (StringReader r = new StringReader("data/newUser.json"))
+            //{
+            //    string json = r.ReadToEnd();
+            //    nUser = JsonConvert.DeserializeObject<List<User>>(json);
+            //}
+            //foreach (var user in nUser)
+            //{
+               
+
+                nUser.Add(new User()
+                {
+                    ID = newUser.ID,
+                    Name = newUser.Name,
+                    FamilyName = newUser.FamilyName,
+                    Age = newUser.Age
+                });
+                string json = JsonConvert.SerializeObject(nUser.ToArray());
+                //string jsonData = JsonConvert.SerializeObject(json, Formatting.None);
+              System.IO.File.WriteAllText(@"C:\Users\User\Desktop\Work\Week 20\WebAPI\WebAPI\data\newUsers.json", json);
+            return newUser;
+        }
+          
+           
+        }
     }
-}
